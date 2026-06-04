8a1d1e65d14573d5b064437479264f12ea3de864
sridharansri <srisridharan738@gmail.com>
2026-06-04 17:04:18 +0530

    pdf

--- src/pages/democatalog/downloadcatalog.tsx
+++ src/pages/democatalog/downloadcatalog.tsx
@@ -36,7 +36,7 @@ export default function Downloadcatalog() {
     {
       title: "E-flyer",
       desc: "Download the complete product catalog including device details and technical specifications.",
-      file: "/tms product/TMS e-flyer.PDF",
+      file: "/tms product/TMS e-flyer.pdf",
       file2: "/pain_managment/Pain Management e-flyer.pdf",
       icon: <Download className="w-6 h-6" />,
     },

e21a64d93f10e15b477296a73e09489122fa0d10
sridharansri <srisridharan738@gmail.com>
2026-06-16 22:47:33 +0530

    outfile commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+    images: {
+    unoptimized: true,
+    
+  },
   reactStrictMode: true,
 };
 
 export default nextConfig;
+
+
+
+
+
+
+
+
+
+ 
+
